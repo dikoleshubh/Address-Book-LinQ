43ac4f1c931d1293919e92b7a735032854cf7ea7
dikoleshubh <dikole.shubh@gmail.com>
2021-05-27 22:10:42 +0530

    ADDRESS BOOK TABLE-RETRIEVE ALL RECORDS -UC-3

--- AddressBookTable.cs
+++ AddressBookTable.cs
@@ -9,7 +9,7 @@ namespace AddressBookLinQ
 {
     class AddressBookTable
     {
-        //Creating DataTable for addressbook problem UC1
+        //Retrive All Records
         DataTable dataTable = new DataTable();
 
         
@@ -24,7 +24,28 @@ namespace AddressBookLinQ
             dataTable.Columns.Add("ZipCode", typeof(int));
             dataTable.Columns.Add("PhoneNumber", typeof(long));
             dataTable.Columns.Add("Email", typeof(string));
+            dataTable.Rows.Add("Arti", "Dandge", "Ashish Plaza", "Pune", "MH", 763222, 9876543210, "asd@gmail.com");
+            dataTable.Rows.Add("Champa", "Magar", "Venu Hights", "Pune", "MH", 763222, 933343210, "rohit@gmail.com");
+            dataTable.Rows.Add("Neha", "Sharma", "Flex Road", "Mumbai", "MH", 403222, 6776543210, "neha@gmail.com");
+            dataTable.Rows.Add("Preeti", "Neghi", "Neer Road", "Benglore", "Karnataka", 40002, 999000880, "preeti@gmail.com");
+            dataTable.Rows.Add("Dameto", "Swami", "Panji", "Panaji", "Goa", 43254, 7777743210, "asd@gmail.com");
+            dataTable.Rows.Add("Rama", "Magare", "Indor", "Indor", "MP", 43254, 7877743990, "Rama@gmail.com");
+            dataTable.Rows.Add("Rekha", "Swami", "baroda", "Baroda", "MP", 43254, 7888743210, "rekha@gmail.com");
 
+        }
+        public void displayAddressBook()
+        {
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Console.WriteLine("\nFirstName:-" + row.Field<string>("FirstName"));
+                Console.WriteLine("LastName:-" + row.Field<string>("LastName"));
+                Console.WriteLine("Address:-" + row.Field<string>("Address"));
+                Console.WriteLine("City:-" + row.Field<string>("City"));
+                Console.WriteLine("State:-" + row.Field<string>("State"));
+                Console.WriteLine("ZipCode:-" + row.Field<int>("ZipCode"));
+                Console.WriteLine("PhoneNumber:-" + row.Field<long>("PhoneNumber"));
+                Console.WriteLine("Email:-" + row.Field<string>("Email"));
+            }
         }
     }
 }
